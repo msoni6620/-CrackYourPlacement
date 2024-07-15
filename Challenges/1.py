3268766015cd45b1945246d99c09b5ee913eebe9
Mayank Soni <110720869+msoni6620@users.noreply.github.com>
2024-07-16 00:11:41 +0530

    Delete Challenges directory

--- Challenges/1.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-def findDuplicate(nums):
-        c=Counter(nums)
-        for key,value in c.items():
-            if(value>=2):
-                return key
-n=list(map(int,input().split()))
-print(findDuplicate(n))
-            
